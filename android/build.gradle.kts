08ef9833e77bf3c5450e0443d7f2300e79d1b2c1
Lol163636363 <152804464+Lol163636363@users.noreply.github.com>
2026-04-02 07:08:38 +0000

    Met à jour le plugin Flutter Gradle pour ne pas l'appliquer par défaut dans le fichier build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,40 +1,12 @@
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply true
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
-android {
-    namespace = "com.example.mamai"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"   // ← version requise par tous les plugins
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    defaultConfig {
-        applicationId = "com.example.mamai"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
-        }
-    }
-}
-
-flutter {
-    source = "../.."
 }
